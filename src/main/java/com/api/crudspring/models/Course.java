28c4fa62d87af685ced13eeaf07be83924bc9cc3
navas <gabrielnavas@email.com>
2023-07-28 17:32:21 -0300

    add constructor without param id

--- src/main/java/com/api/crudspring/models/Course.java
+++ src/main/java/com/api/crudspring/models/Course.java
@@ -23,4 +23,9 @@ public class Course {
 
     @Column(nullable = false,length = 15)
     private String category;
+
+    public Course(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
 }
