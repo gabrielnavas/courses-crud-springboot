ffb7bc045703b0b31d8efc38caf7127e92cf0ee8
navas <gabrielnavas@email.com>
2023-07-28 19:41:56 -0300

    add cors to localhost, configure database h2 and jpa

--- src/main/java/com/api/crudspring/controllers/CourseController.java
+++ src/main/java/com/api/crudspring/controllers/CourseController.java
@@ -3,16 +3,14 @@ package com.api.crudspring.controllers;
 import com.api.crudspring.models.Course;
 import com.api.crudspring.repositories.CourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 public class CourseController {
 
     private final CourseRepository courseRepository;
